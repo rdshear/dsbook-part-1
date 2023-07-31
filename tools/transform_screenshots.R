acb8dccbe797eb1b5e325029bb32cf8021d9ea95
Robert D Shear <rshear@gmail.com>
2023-07-31 06:05:41 -0400

    WIP

--- tools/transform_screenshots.R
+++ tools/transform_screenshots.R
@@ -3,24 +3,32 @@
 library(tidyverse)
 library(codetools)
 
+# TODO Construct a code walker that returns the list, rewritten
 cw <- codetools::makeCodeWalker(call = function (e, w) {
-    result <- NULL
     if (mode(e) == "call" && identical(e[[1]], target_call)) {
-      result <- deparse(e)
+      return(e)
     }
     for (ee in as.list(e)) {
       if (!missing(ee)) 
         walkCode(ee, w)
     }
-    return(result)
   },
+  handler = function(v, w) {str(v); browser(); NULL},
   leaf = function(e, w) NULL)
 
 
 loc <- "~/Projects/dsbook-part-1/R/getting-started.qmd"
 
 src <- readLines(loc)
-fences <-  which(str_starts(src, '```'))
+
+# ignore quadtick chunks
+quadticks <- cumsum(str_starts(src, '````')) %% 2 == 1
+quadticks <- !(quadticks | lag(quadticks, default = FALSE))
+if (!tail(quadticks, 1)) {
+  stop("Unbalanced quadticks (````)")
+}
+
+fences <-  which(str_starts(src, '```') & quadticks)
 if (length(fences) %% 2 != 0) {
   stop("Unbalanced chunk fences")
 }
@@ -45,7 +53,8 @@ target_call = quote(knitr::include_graphics)
 apply(chunks[chunks$lan == "r",], 1, (\(df) {
   code <- src[(df$start + 1):(df$end - 1)]
   cat(sprintf("Parsing lines %d-%d\n", df$start, df$end))
-  ptree <- tryCatch(parse(text = code, keep.source = TRUE),
+
+    ptree <- tryCatch(parse(text = code, keep.source = TRUE),
                 error = function(e) {
                   print(e)
                   return(NULL)
@@ -57,9 +66,9 @@ apply(chunks[chunks$lan == "r",], 1, (\(df) {
   {
     walk_result <- codetools::walkCode(ptree[[1]], cw)
     if (!is.null(walk_result)) {
-      # TODO: NEW STUFF GOES HERE
+      # TODO DEBUG ONLY
       # browser()
-      cat("Walk:",walk_result, "\n")
+      cat("Walk:",deparse(walk_result), "\n")
     }
   }
   # cat(sprintf("----%d,%d ... %s\n", df$start, df$end, df$params))
